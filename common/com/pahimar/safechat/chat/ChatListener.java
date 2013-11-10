30df95e1b57969644edb4b4bd21a9e80d076974b
pahimar <pahimar@gmail.com>
2013-11-09 23:06:59 -0500

    Some work on filtering

--- common/com/pahimar/safechat/chat/ChatListener.java
+++ common/com/pahimar/safechat/chat/ChatListener.java
@@ -1,5 +1,12 @@
 package com.pahimar.safechat.chat;
 
+import java.util.Iterator;
+
+import com.pahimar.safechat.blacklist.BlackList;
+import com.pahimar.safechat.configuration.Settings;
+import com.pahimar.safechat.helper.LogHelper;
+import com.pahimar.safechat.lib.Reference;
+
 import net.minecraft.network.packet.NetHandler;
 import net.minecraft.network.packet.Packet3Chat;
 import cpw.mods.fml.common.network.IChatListener;
@@ -24,7 +31,35 @@ public class ChatListener implements IChatListener {
     @Override
     public Packet3Chat serverChat(NetHandler handler, Packet3Chat message) {
 
-        return null;
+        if (Settings.FILTER_MODE > Reference.FILTER_MODE_NONE) {
+            if (message.message.startsWith("/msg") || message.message.startsWith("/me") || !message.message.startsWith("/")) {
+                LogHelper.debug("Server received a chat message that could be filtered message");
+                
+                if (Settings.FILTER_MODE == Reference.FILTER_MODE_REPLACE) {
+                    
+                }
+                else if (Settings.FILTER_MODE == Reference.FILTER_MODE_HIDE) {
+                    
+                    boolean blackListWordFound = false;
+                    String messageLowerCase = message.message.toLowerCase();
+                    
+                    Iterator<String> blackListIterator = BlackList.getBlackList().iterator();
+                    String blackListWord = null;
+                    while (!blackListWordFound && blackListIterator.hasNext()) {
+                        blackListWord = blackListIterator.next();
+                        if (messageLowerCase.contains(blackListWord.toLowerCase())) {
+                            blackListWordFound = true;
+                        }
+                    }
+                    
+                    if (blackListWordFound) {
+                        message.message = "[Message removed by SafeChat]";
+                        return message;
+                    }
+                }
+            }
+        }
+        return message;
     }
 
     /*
@@ -36,6 +71,6 @@ public class ChatListener implements IChatListener {
     @Override
     public Packet3Chat clientChat(NetHandler handler, Packet3Chat message) {
 
-        return null;
+        return message;
     }
 }
