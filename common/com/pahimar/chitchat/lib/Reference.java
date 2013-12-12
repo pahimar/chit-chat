0dc8978bf414d204dd98904fc9ebe6e63afacfc4
bspkrs <bspkrs@gmail.com>
2013-12-12 01:06:14 -0500

    Added a bunch of ASCII characters to the equivalent characters map to be proactive

--- common/com/pahimar/chitchat/lib/Reference.java
+++ common/com/pahimar/chitchat/lib/Reference.java
@@ -75,31 +75,31 @@ public class Reference {
      *  Initializes the equivalent character map, mapping common letter substitutions to letters of the alphabet 
      */
     private static void initEquivalentCharacterMaps() {
-        Reference.equivalentCharacterMap.put("a", Arrays.asList("@", "4"));
-        Reference.equivalentCharacterMap.put("b", Arrays.asList("8"));
-        Reference.equivalentCharacterMap.put("c", null);
-        Reference.equivalentCharacterMap.put("d", null);
-        Reference.equivalentCharacterMap.put("e", Arrays.asList("3"));
-        Reference.equivalentCharacterMap.put("f", null);
+        Reference.equivalentCharacterMap.put("a", Arrays.asList("@", "4", "æ", "à", "á", "â", "â", "ã", "ä", "å", "À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "ª"));
+        Reference.equivalentCharacterMap.put("b", Arrays.asList("8", "ß", "Þ", "þ"));
+        Reference.equivalentCharacterMap.put("c", Arrays.asList("ç", "Ç", "©", "¢", "Œ"));
+        Reference.equivalentCharacterMap.put("d", Arrays.asList("Ð"));
+        Reference.equivalentCharacterMap.put("e", Arrays.asList("3", "æ", "è", "é", "ê", "ë", "€", "È", "É", "Ê", "Ë", "Æ", "Œ", "œ"));
+        Reference.equivalentCharacterMap.put("f", Arrays.asList("ƒ"));
         Reference.equivalentCharacterMap.put("g", null);
         Reference.equivalentCharacterMap.put("h", null);
-        Reference.equivalentCharacterMap.put("i", Arrays.asList("!", "l", "1"));
+        Reference.equivalentCharacterMap.put("i", Arrays.asList("!", "l", "1", "ì", "í", "î", "ï", "Ì", "Í", "Î", "Ï", "¡"));
         Reference.equivalentCharacterMap.put("j", null);
         Reference.equivalentCharacterMap.put("k", null);
-        Reference.equivalentCharacterMap.put("l", Arrays.asList("1"));
+        Reference.equivalentCharacterMap.put("l", Arrays.asList("1", "£"));
         Reference.equivalentCharacterMap.put("m", null);
-        Reference.equivalentCharacterMap.put("n", null);
-        Reference.equivalentCharacterMap.put("o", Arrays.asList("0"));
-        Reference.equivalentCharacterMap.put("p", null);
+        Reference.equivalentCharacterMap.put("n", Arrays.asList("ñ", "Ñ"));
+        Reference.equivalentCharacterMap.put("o", Arrays.asList("0", "ð", "ò", "ó", "ô", "õ", "ö", "ø", "Ò", "Ó", "Ô", "Õ", "Ö", "Ø", "œ"));
+        Reference.equivalentCharacterMap.put("p", Arrays.asList("Þ", "þ"));
         Reference.equivalentCharacterMap.put("q", null);
-        Reference.equivalentCharacterMap.put("r", null);
-        Reference.equivalentCharacterMap.put("s", Arrays.asList("5","$", "z"));
-        Reference.equivalentCharacterMap.put("t", Arrays.asList("+", "7"));
-        Reference.equivalentCharacterMap.put("u", null);
+        Reference.equivalentCharacterMap.put("r", Arrays.asList("®"));
+        Reference.equivalentCharacterMap.put("s", Arrays.asList("5","$", "z", "Š", "š", "§"));
+        Reference.equivalentCharacterMap.put("t", Arrays.asList("+", "7", "†"));
+        Reference.equivalentCharacterMap.put("u", Arrays.asList("ù", "ú", "û", "ü", "Ù", "Ú", "Û", "Ü"));
         Reference.equivalentCharacterMap.put("v", null);
         Reference.equivalentCharacterMap.put("w", null);
-        Reference.equivalentCharacterMap.put("x", null);
-        Reference.equivalentCharacterMap.put("y", null);
-        Reference.equivalentCharacterMap.put("z", Arrays.asList("2", "s"));
+        Reference.equivalentCharacterMap.put("x", Arrays.asList("×"));
+        Reference.equivalentCharacterMap.put("y", Arrays.asList("ý", "ÿ", "Ý", "¥", "Ÿ"));
+        Reference.equivalentCharacterMap.put("z", Arrays.asList("2", "s", "Ž", "ž"));
     }
 }
